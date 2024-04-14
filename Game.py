768bfd6b1a66518500d2c5306ad918ab2255c991
Maximiliano <maxvillarroel@outlook.com>
2024-04-14 02:34:24 -0400

    Se implementa flota enemiga de multiples filas vía comprensión de lista.

--- Game.py
+++ Game.py
@@ -11,15 +11,11 @@ class Game(GameWorld):
         # Inicializamos el mundo del juego
         super().__init__(800, 600, title="Actividad 01", bgpic="Recursos/Fondo.png")
 
+        
         # agregamos a los actores
         SpaceShip(400, 540)
-        Alien(100, 50)
-        Alien(200, 50)
-        Alien(300, 50)
-        Alien(400, 50)
-        Alien(500, 50)
-        Alien(600, 50)
-        Alien(700, 50)
+        self.aliens = [Alien(i, j) for i in range(100, 800, 100) for j in (50, 150, 250)]
+
 
     def onUpdate(self, dt):
         fps = round(1 / dt, 1)
